7d912015afd30150fde4c3b8ff4ac6222e3113c5
tylim <limteikyee@gmail.com>
2022-12-26 21:32:59 +0800

    update prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    singleQuote: true,
+    printWidth: 120,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    trailingComma: 'none',
+    tabWidth: 4,
+    semi: false,
+    endOfLine: 'auto'
+}
