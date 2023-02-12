9f79170b746c4188f1b1d2deb47353fe48602a84
tylim <limteikyee@gmail.com>
2023-02-13 07:27:17 +0800

    lot of changes but I forgot why I changed them, so...

--- packages/node-docker/src/main.ts
+++ packages/node-docker/src/main.ts
@@ -1,6 +1,15 @@
-import * as http from 'http'
+import { initTRPC } from '@trpc/server'
+import { fibonacciInput } from '@myOrg/node-lib'
+import { fibonacci } from './app'
 
-const server = http.createServer((request, response) => {
-    response.end('Hello, World')
+const t = initTRPC.create()
+
+export const appRouter = t.router({
+    fibonacci: t.procedure.input(fibonacciInput).query(({ input }) => {
+        return {
+            output: fibonacci(input.n)
+        }
+    })
 })
-server.listen(3100)
+
+export type AppRouter = typeof appRouter
